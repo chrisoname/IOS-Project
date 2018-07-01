e260941d7bfecbde4c8abfc75d5c230eb3b84d02
Christopher De Jesus <chrisoname@gmail.com>
2018-07-01 12:47:04 -0400

    Setting up cards, emojis in cards with view controllers and a counter of flips

--- Cards Memory/ViewController.swift
+++ Cards Memory/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount  = 0 {
+        didSet {
+            flipCountLabel.text  =  "Flips: \(flipCount)"
+        }
     }
+    
+    var emojiChoices = ["🐊", "🐖", "🐖","🐊"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = collectionOfCards.index(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    @IBOutlet var collectionOfCards: [UIButton]!
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton ){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.4218907992, blue: 0.06438108498, alpha: 1)
+        }
+        else {
+        button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.15317265, blue: 0.5132747033, alpha: 1)
+        }
+    }
+    
+   
+    
 
 }
 
